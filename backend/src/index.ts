0555f3c71dbb1a3b36f1ffd8870f37748ee5670e
vitamin777 <rlaalsdn8@gmail.com>
2023-03-16 00:03:31 +0900

    feat: express server setting & connection
    -1- src/index.ts 파일 생성 후 익스프레스 서버 미들웨어 설정 코드 작성
    (express.json(), express.urlencoded(), cors(), helmet())
    -1-02- port number 는 .env 에 4000 번 포트 사용하도록 함 연결 실패시 5000 번 포트 사용
    -1-03- 404 에러 처리 (요청 페이지 찾을 수 없음 메시지 출력)
    
    -2- DB 연결 함수 runDB 코드 작성 (연결 성공 및 실패 콘솔 출력)
    (mongoDB 에서 데이터베이스 생성 후 DB URI 는 .env 에 작성)
    
    port number 4000 연결 성공 및 MongoDB 연결 성공 확인

---
+++ backend/src/index.ts
@@ -0,0 +1,44 @@
+import * as dotenv from "dotenv";
+dotenv.config();
+import express, { Request, Response, Application } from "express";
+import cors from "cors";
+import helmet from "helmet";
+import { connect } from "mongoose";
+
+// 익스프레스 에플리케이션 생성
+const app: Application = express();
+
+// 미들웨어 설정
+// express.json() 미들웨어는 application/json 형식의 데이터를 분석하여 req.body 객체로 만들어준다.
+app.use(express.json());
+// express.urlencoded() 미들웨어는 application/x-www-form-urlencoded 형식의 데이터를 분석하여 req.body 객체로 만들어준다.
+// extended: true 옵션을 주면 qs 모듈을 사용하고, false 옵션을 주면 querystring 모듈을 사용한다.
+// qs 모듈은 내장 모듈이 아니므로 따로 설치해야 한다.
+app.use(express.urlencoded({ extended: true }));
+// cors 미들웨어는 클라이언트에서 요청을 보낼 때 CORS(Cross-Origin Resource Sharing) 관련 설정을 할 수 있게 해준다.
+app.use(cors());
+// helmet 미들웨어는 애플리케이션의 보안을 강화해준다.
+app.use(helmet());
+
+const PORT = (process.env.PORT as unknown as number) || 5000;
+
+// 404 에러 처리
+app.all("*", (req: Request, res: Response) => {
+  res.status(404).json({
+    message: "요청하신 페이지를 찾을 수 없습니다.",
+  });
+});
+
+// DB 연결 함수
+const runDB = async () => {
+  connect(process.env.MONGODB_URI as string)
+    .then(() => console.log("MongoDB 연결 성공"))
+    .catch(() => console.log("MongoDB 연결 실패"));
+};
+
+runDB();
+
+// port 를 얻을 때마다 호출할 함수를 전달
+app.listen(PORT, () => {
+  console.log(`현재 실행되고있는 서버의 포트 넘버: ${PORT}`);
+});
