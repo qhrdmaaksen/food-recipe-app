8da2579ffa7a86f1d5d05cb2be7fcb41b4189c94
vitamin777 <rlaalsdn8@gmail.com>
2023-04-10 23:58:50 +0900

    feat: 모든 레시피, 단일 레시피 가져오기 추가

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -15,27 +15,9 @@ J
 backend/src/model/index.ts,3\9\392ee5521231283322d11918923f26d63c4f48f6
 K
 backend/src/model/recipe.ts,2\1\21252ac434155d586059b68208ceaf4f8cc26767
-I
-backend/src/model/user.ts,f\1\f1daeec28db7c9bf6101d689624f0e4620334374
 K
 backend/src/routes/index.ts,f\f\ffa12e4050dd01b6a999ca4ce4c0c13c911fc3c5
-L
-backend/src/routes/recipe.ts,0\6\060942cc7f53c20811a55e64feedfc3e227f46fd
-D
-backend/src/index.ts,c\a\cadccc3f718c67fe80df9a5bc7dcc79a0a032c23
 <
 backend/.env,f\3\f30451335edfb80e231f658d0defc83bb7e4c3a7
-J
-backend/src/routes/auth.ts,b\e\be2310e742f999a225d4d679ee3af5bc43afa50c
-W
-'backend/src/schema-validations/index.ts,a\f\af6b67d660f587d90129abd8f2a906ba458b940c
-O
-backend/src/controllers/auth.ts,4\f\4f55cdc6771f526e33b3936bb4cf8008afa21c15
 B
 backend/.gitignore,7\e\7e16691a9fc60084a157565ec422c8094d3df67c
-R
-"backend/src/constants/constants.ts,0\6\060b34a076f39f3c9155a75534862bf0eb2ad1bc
-N
-backend/src/constants/index.ts,6\b\6bb70d6fa6c7012a54f917abee03bf702b8ccc39
-Q
-!backend/src/controllers/recipe.ts,e\5\e5ed9dd89f8eff40af931a7f7fef74f9c49094d6
--- backend/src/controllers/recipe.ts
+++ backend/src/controllers/recipe.ts
@@ -1,10 +1,48 @@
 import { Request, Response } from "express";
 import { Recipe } from "src/model";
+
+// 모든 레시피 정보를 가져오는 함수
 export const getAllRecipes = async (req: Request, res: Response) => {
   try {
-      // populate() 메서드를 사용하여 user 필드를 채워준다.
-      // populate() 메서드는 첫 번째 인자로 참조하는 필드를, 두 번째 인자로 해당 필드에 대한 정보를 가져올 필드를 받는다.
-      // sort() 메서드를 사용하여 _id 필드를 기준으로 내림차순으로 정렬한다.
-    const recipes = await Recipe.find({}).populate("user", "email").sort({_id: -1});
-  } catch (error) {}
+    // populate() 메서드를 사용하여 user 필드를 채워준다.
+    // populate() 메서드는 첫 번째 인자로 참조하는 필드를, 두 번째 인자로 해당 필드에 대한 정보를 가져올 필드를 받는다.
+    // sort() 메서드를 사용하여 _id 필드를 기준으로 내림차순으로 정렬한다.
+    const recipes = await Recipe.find({})
+      .populate("user", "email")
+      .sort({ _id: -1 })
+      .exec();
+
+    return res.status(200).json(recipes);
+  } catch (error) {
+    console.log(error);
+    res
+      .status(500)
+      .json({
+        error: "모든 레시피 가져오기 요청을 처리하는 동안 오류가 발생했습니다.",
+      });
+  }
+};
+
+// 특정 레시피 정보를 가져오는 함수
+export const getRecipe = async (req: Request, res: Response) => {
+  const { id } = req.params;
+  try {
+    const recipe = await Recipe.findById(id).populate("user", "email").exec();
+
+    // 해당 레시피가 존재하지 않는 경우
+    if (!recipe) {
+      return res
+        .status(404)
+        .json({ error: "해당 레시피가 존재하지 않습니다." });
+    }
+
+    res.status(200).json(recipe);
+  } catch (error) {
+    console.log(error);
+    res
+      .status(500)
+      .json({
+        error: "특정 레시피 가져오기 요청을 처리하는 동안 오류가 발생했습니다.",
+      });
+  }
 };
