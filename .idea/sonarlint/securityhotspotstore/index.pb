984796b4b3d26c02f3c331e270866f538c8e0b13
vitamin777 <rlaalsdn8@gmail.com>
2023-04-25 22:18:13 +0900

    feat: server 작업 완료 및 post req,res 테스트 완료
    - schema type 재정의 : any 타입에서 각 createRecipeSchema, getRecipeSchema, getUserRecipesSchema, joinSchema 타입 정의
    
    - 로그인 테스트 완료
    - 로그인 토큰 발급 테스트 완료
    - 로그인 하지 않은 유저 경로 제한 테스트 완료
    - 로그인 계정 유효성 검사 테스트 완료
    - 레시피 생성 테스트 완료
    - 레시피 불러오기 테스트 완료
    - 레시피 검색 테스트 완료
    - 레시피 필수 항목 미기입 테스트 완료

--- .idea/sonarlint/securityhotspotstore/index.pb
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -1,10 +1,6 @@
 
 E
 backend/tsconfig.json,2\d\2de55f36b9608859c02740cfa56b943f6eff3a73
-P
- backend/src/middlewares/index.ts,8\5\854276991a6b1bc33881c62c430ff87c86ddd46c
-S
-#backend/src/middlewares/validate.ts,7\1\71825620664755bb5f00a711c4ad0945d787c92f
 B
 backend/.gitignore,7\e\7e16691a9fc60084a157565ec422c8094d3df67c
 J
@@ -33,3 +29,5 @@ K
 backend/.env,f\3\f30451335edfb80e231f658d0defc83bb7e4c3a7
 F
 backend/yarn-error.log,a\2\a21fefc96740bc8af9da531c7dacc01066f6a6a1
+D
+backend/src/index.ts,c\a\cadccc3f718c67fe80df9a5bc7dcc79a0a032c23
--- backend/src/middlewares/validate.ts
+++ backend/src/middlewares/validate.ts
@@ -3,19 +3,36 @@
 // NextFunction 은 다음 미들웨어를 실행시켜주는 함수
 import { NextFunction, Request, Response } from "express";
 // InferType 은 yup 의 타입을 추론해주는 함수
+import { InferType } from "yup";
+import {
+  createRecipeSchema,
+  getRecipeSchema,
+  getUserRecipesSchema,
+  joinSchema,
+} from "../schema-validations";
 
 // 요청 데이터의 유효성을 검사하는 미들웨어
 export const validate =
   // schema 는 yup 의 스키마 객체 (mongodb 의 스키마가 아님)
-  (schema: any) => async (req: Request, res: Response, next: NextFunction) => {
-    try {
-      await schema.validate({
-          ...(req?.body && {body: req.body}),
-          ...(req?.query && {query: req.query}),
-          ...(req?.params && {params: req.params}),
-      });
-      return next();
-    } catch (err: any) {
-      return res.status(400).json({ error: err.message });
-    }
-  };
+  // yup 의 스키마 객체는 다음과 같이 생성함
+
+    (
+      schema: InferType<
+        | typeof createRecipeSchema
+        | typeof getRecipeSchema
+        | typeof getUserRecipesSchema
+        | typeof joinSchema
+      >
+    ) =>
+    async (req: Request, res: Response, next: NextFunction) => {
+      try {
+        await schema.validate({
+          ...(req?.body && { body: req.body }),
+          ...(req?.query && { query: req.query }),
+          ...(req?.params && { params: req.params }),
+        });
+        return next();
+      } catch (err: any) {
+        return res.status(400).json({ error: err.message });
+      }
+    };
