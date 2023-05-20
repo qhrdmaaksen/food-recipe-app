b1a5b067a4101a745cf0f51021dbaef64fc74d73
vitamin777 <rlaalsdn8@gmail.com>
2023-05-20 22:28:55 +0900

    feat: 사이드바 추가 및 css 작업중
    - side bar layout 추가, 사용자 이미지 및 정보 더미 데이터 추가 테스트중

--- frontend/src/context/auth-context.tsx
+++ frontend/src/context/auth-context.tsx
@@ -2,8 +2,10 @@ import { createContext, ReactNode, useEffect, useState } from "react";
 import { AUTH_TYPE } from "../@types";
 import { useAuth } from "../hooks";
 
+// AuthenticationContext 를 생성한다.
 export const AuthenticationContext = createContext<AUTH_TYPE | null>(null);
 
+// AuthenticationContextProvider 를 생성 후 children 을 받는다.
 export const AuthenticationContextProvider = ({
   children,
 }: {
@@ -15,16 +17,18 @@ export const AuthenticationContextProvider = ({
   useEffect(() => {
     const token = sessionStorage.getItem("token");
     const storedUser = sessionStorage.getItem("email");
-
+    // sessionStorage 에 저장된 토큰과 이메일이 존재하는 경우 setUser 를 통해 storeUser 를 설정한다.
     if (storedUser !== undefined && storedUser !== null && token) {
       setUser(storedUser);
     }
   }, []);
 
+  // onLogin 은 payload 를 받아서 login 을 실행한다.
   const onLogin = async (payload: {
     email: string;
     password: string;
   }): Promise<unknown> => {
+    // response 는 login 의 응답을 받는다.
     const response = await login(payload);
     if (response) {
       // 응답이 null 이 아닌 경우 sessionStorage 에 토큰, 이메일, 아이디를 저장한다.
@@ -32,6 +36,7 @@ export const AuthenticationContextProvider = ({
       sessionStorage.setItem("email", response?.email);
       sessionStorage.setItem("id", response?.id);
 
+      // setUser 로 응답 받은 이메일을 설정한다.
       setUser(response?.email);
       // 로그인 성공 시 dashboard 로 이동한다.
       return (window.location.href = "/dashboard");
--- frontend/src/layouts/dashboard.tsx
+++ frontend/src/layouts/dashboard.tsx
@@ -1,31 +1,65 @@
-import {useContext, useLayoutEffect} from "react";
-import {AuthenticationContext} from "../context";
-import {AUTH_TYPE} from "../@types";
-import {NavLink, useNavigate} from "react-router-dom";
+import { useContext, useLayoutEffect } from "react";
+import { AuthenticationContext } from "../context";
+import { AUTH_TYPE } from "../@types";
+import { NavLink, useNavigate, useParams } from "react-router-dom";
 
 const routes = [
-    {name: 'home', to: '/dashboard'},
-    {name: 'Add Recipe', to: '/dashboard/addrecipe'},
-    {name: 'My Recipes', to: '/dashboard/myrecipes'},
-]
+  { name: "home", to: "/dashboard" },
+  { name: "Add Recipe", to: "/dashboard/addrecipe" },
+  { name: "My Recipes", to: "/dashboard/myrecipes" },
+];
 
 export const DashboardLayout = () => {
-    const navigate = useNavigate()
-    useLayoutEffect(() => {
-        // 로그인이 되어있지 않은 경우 landing page 로 이동한다.
-        if(!sessionStorage.getItem('email') && !sessionStorage.getItem('token')) {
-            navigate('/');
-        }
-    }, [])
-    const {onLogout, loading} = useContext(AuthenticationContext) as AUTH_TYPE;
-    return <div className='w-full h-full bg-black overflow-x-hidden'>
-        <div className='h-[60px] md:h-[80px] bg-zinc-900 flex items-center justify-between px-3 sticky top-0 z-50'>
-            <div className='flex items-center'>
-                <h2 className='text-white font-bold text-xl underline-offset-4 underline'>
-                    <NavLink to='/dashboard'>Foodie</NavLink>
-                </h2>
-                <span className='text-orange-700' font-extrabold text-xl pl-2>.</span>
-            </div>
+  const navigate = useNavigate();
+  // useParams 를 통해 현재 path 를 가져온다.
+  const pathname = useParams().pathname;
+  // 렌더링 이후 로그인이 되어있지 않은 경우 landing page 로 이동한다.
+  useLayoutEffect(() => {
+    if (!sessionStorage.getItem("email") && !sessionStorage.getItem("token")) {
+      navigate("/");
+    }
+  }, []);
+  // onLogout, loading 은 AuthenticationContext 에서 가져온다.
+  const { onLogout, loading } = useContext(AuthenticationContext) as AUTH_TYPE;
+  return (
+    <div className="w-full h-full bg-black overflow-x-hidden">
+      <div className="h-[60px] md:h-[80px] bg-zinc-900 flex items-center justify-between px-3 sticky top-0 z-50">
+        <div className="flex items-center">
+          <h2 className="text-white font-bold text-xl underline-offset-4 underline">
+            <NavLink to="/dashboard">Foodie</NavLink>
+          </h2>
+          <span className="text-orange-700 font-extrabold text-xl pl-2">.</span>
         </div>
+      </div>
+      <div className="flex flex-col md:flex-row w-full h-full z-10 relative">
+        <div className="hidden md:block bg-zinc-900 h-full w-[20%] fixed">
+          <div className="md:flex gap-8 items-start w-full p-3">
+            <img
+              className="h-16 w-16 object-cover rounded-full"
+              src="https://images.unsplash.com/photo-1612766959025-ac18e2b3bb96?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80"
+              alt="A image"
+            />
+            <div>
+              <p className="text-orange-500 font-light">krman@gmail.com</p>
+            </div>
+          </div>
+        </div>
+        <div className="flex flex-col gap-y-1 mt-3">
+          {routes.map(({ name, to }) => (
+            <NavLink
+              key={name + to}
+              to={to}
+              className={({ isActive }) =>
+                isActive && pathname === to
+                  ? "text-white font-thin text-sm bg-orange-500 p-4"
+                  : "text-white font-thin text-sm hover:bg-orange-500 p4"
+              }
+            >
+              {name}
+            </NavLink>
+          ))}
+        </div>
+      </div>
     </div>
-}
+  );
+};
